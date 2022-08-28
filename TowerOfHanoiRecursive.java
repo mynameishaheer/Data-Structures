34c8124db31ab6854fa4b29a42f7331a0bc216da
Shaheer Ahmed <94866222+mynameishaheer@users.noreply.github.com>
2022-08-28 22:35:49 +0500

    Update TowerOfHanoiRecursive.java

--- TowerOfHanoiRecursive.java
+++ TowerOfHanoiRecursive.java
@@ -1,25 +1,24 @@
 
 public class HanoiRec {
-	
+
 	static int count;
 
-	public static void tower(int n, char from, char to, char aux){
+	public static void tower(int n, char from, char to, char aux) {
 		count++;
-		
-		if (n ==1) {
+
+		if (n == 1) {
 			System.out.println("Move from: " + from + " to " + to);
 			return;
-		}
-		else {
-		tower(n-1, from, aux, to);
-		System.out.println("Move from: " + from + " to " + to);
-		tower(n-1, aux, to, from);
+		} else {
+			tower(n - 1, from, aux, to);
+			System.out.println("Move from: " + from + " to " + to);
+			tower(n - 1, aux, to, from);
 		}
 	}
-	
-	public static void main (String[] args) {
-		tower(6,'A', 'C','B');
-		System.out.println("Total recursive calls made: " + (count-1));
+
+	public static void main(String[] args) {
+		tower(6, 'A', 'C', 'B');
+		System.out.println("Total recursive calls made: " + (count - 1));
 	}
-	
+
 }
