bafc9f02c403c32a222cd73a061e9a114ca9248f
Shaheer Ahmed <94866222+mynameishaheer@users.noreply.github.com>
2022-08-28 22:34:42 +0500

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -1,109 +1,109 @@
 public class LList {
 
-  static class node {
+	static class node {
 
-    int data;
-    node pointer;
+		int data;
+		node pointer;
 
-    node(int d) {
-      data = d;
-      pointer = null;
-    }
-  }
+		node(int d) {
+			data = d;
+			pointer = null;
+		}
+	}
 
-  static class LinkedList {
+	static class LinkedList {
 
-    node start;
+		node start;
 
-    public void insert(int d) {
-      node n = new node(d);
+		public void insert(int d) {
+			node n = new node(d);
 
-      if (start == null) {
-        start = n;
-        System.out.println("Item Inserted: " + d);
-      } else {
-        node temp = start;
-        while (temp.pointer != null) {
-          temp = temp.pointer;
-        }
+			if (start == null) {
+				start = n;
+				System.out.println("Item Inserted: " + d);
+			} else {
+				node temp = start;
+				while (temp.pointer != null) {
+					temp = temp.pointer;
+				}
 
-        temp.pointer = n;
-        System.out.println("Item Inserted: " + d);
-      }
-    }
+				temp.pointer = n;
+				System.out.println("Item Inserted: " + d);
+			}
+		}
 
-    public Boolean find(int d) {
-      node temp = start;
+		public Boolean find(int d) {
+			node temp = start;
 
-      while (temp != null) {
-        if (temp.data == d) {
-          System.out.println("Item Found: " + d);
-          return true;
-        }
+			while (temp != null) {
+				if (temp.data == d) {
+					System.out.println("Item Found: " + d);
+					return true;
+				}
 
-        temp = temp.pointer;
-      }
+				temp = temp.pointer;
+			}
 
-      System.out.println("Item Not Found");
-      return false;
-    }
+			System.out.println("Item Not Found");
+			return false;
+		}
 
-    public void clear() {
-      start = null;
-      System.out.println("List Cleared");
-    }
+		public void clear() {
+			start = null;
+			System.out.println("List Cleared");
+		}
 
-    public void delete(int d) {
-      node temp = start;
-      node prev = start;
+		public void delete(int d) {
+			node temp = start;
+			node prev = start;
 
-      if (temp != null && temp.data == d) {
-        start = temp.pointer;
-      }
+			if (temp != null && temp.data == d) {
+				start = temp.pointer;
+			}
 
-      while (temp != null && temp.data != d) {
-        prev = temp;
-        temp = temp.pointer;
-      }
+			while (temp != null && temp.data != d) {
+				prev = temp;
+				temp = temp.pointer;
+			}
 
-      if (temp == null) {
-        temp = temp.pointer;
-      }
+			if (temp == null) {
+				temp = temp.pointer;
+			}
 
-      prev.pointer = temp.pointer;
-      System.out.println("Item Deleted: " + d);
-    }
+			prev.pointer = temp.pointer;
+			System.out.println("Item Deleted: " + d);
+		}
 
-    public String toString() {
-      String str = "";
-      node temp = start;
+		public String toString() {
+			String str = "";
+			node temp = start;
 
-      while (temp != null) {
-        str = str + temp.data;
-        System.out.println();
-        temp = temp.pointer;
-      }
+			while (temp != null) {
+				str = str + temp.data;
+				System.out.println();
+				temp = temp.pointer;
+			}
 
-      return str;
-    }
-  }
+			return str;
+		}
+	}
 
-  public static void main(String[] args) {
-    LinkedList list = new LinkedList();
+	public static void main(String[] args) {
+		LinkedList list = new LinkedList();
 
-    list.insert(1);
-    list.insert(3);
-    list.insert(4);
-    list.insert(6);
-    list.insert(7);
+		list.insert(1);
+		list.insert(3);
+		list.insert(4);
+		list.insert(6);
+		list.insert(7);
 
-    System.out.println();
-    list.find(1);
-    System.out.println();
-    list.delete(6);
+		System.out.println();
+		list.find(1);
+		System.out.println();
+		list.delete(6);
 
-    // list.clear();
+		// list.clear();
 
-    System.out.print(list);
-  }
+		System.out.print(list);
+	}
 }
